a1a564ea91e640e87a7d340ed784928841518a42
Sem Schilder <me@sem.design>
2016-05-20 14:55:52 +0100

    init

---
+++ Wishlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wishlist
+//
+//  Created by Sem Schilder on 20/05/2016.
+//  Copyright © 2016 D3 - Creative Agency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
